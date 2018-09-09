e475bb746c544e0016d754d8c971a75beb95ca88
younjin <github@kimyounjin.com>
2018-09-09 12:48:31 -0400

    One.

--- src/One.java
+++ src/One.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class One {
+    public static void main(String[] args) {
+        System.out.println("The sum for n=1000 is " + eulerOne(1000));
+    }
+
+    public static int eulerOne(int n) {
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
 }
